e62e05fe7a3173e4e9eebf9e2471c971f8d89974
songfangyuan <songfangyuan@zto.cn>
2018-09-25 13:06:38 +0800

    测试git

--- test/HTTP协议原理+实践 Web开发工程师必学/http/serve.js
+++ test/HTTP协议原理+实践 Web开发工程师必学/http/serve.js
@@ -1,15 +1,28 @@
-const http = require("http")
+ const http = require("http")
 const fs = require("fs")
 
 http.createServer((request,response) => {
   console.log("request come",request.url)
   
-  const html = fs.readFileSync("test.html","utf8")
-  response.writeHead(200,{
-    // node自动添加
-    "Content-Type" : "text/html"
-  })
-  response.end(html)
+  
+  if(request.url === "/"){
+    const html = fs.readFileSync("test.html","utf8")
+    response.writeHead(200,{
+      // node自动添加
+      "Content-Type" : "text/html"
+    })
+    response.end(html)
+  }
+  
+  if(request.url === "/script.js"){
+    const html = fs.readFileSync("test.html","utf8")
+    response.writeHead(200,{
+      // node自动添加
+      "Content-Type" : "text/javascript",
+    })
+    response.end('console.log("script loaded")')
+  }
+  
 }).listen(8887)
 
 console.log("listening")
--- test/HTTP协议原理+实践 Web开发工程师必学/http/serve2.js
+++ test/HTTP协议原理+实践 Web开发工程师必学/http/serve2.js
@@ -3,7 +3,7 @@ const http = require("http")
 http.createServer((request,response) => {
   console.log("request come",request.url)
   response.writeHead(200,{
-    "Access-Control-Allow-Origin" : "*"
+    "Access-Control-Allow-Origin" : "http://127.0.0.1:8887/"
   })
   response.end("123")
 }).listen(8888)
